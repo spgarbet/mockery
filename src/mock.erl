2fb3164c9659d04310c8791f77b283483349f3ac
Shawn Garbett <sgarbett@changehealthcare.com>
2014-12-21 16:42:50 -0600

    Simplificaiton refactor complete, not tested

--- src/mock.erl
+++ src/mock.erl
@@ -11,37 +11,31 @@
 % Verb, look into prop list to map between cowboy verb and needs here
 
 % Hash method
-urlsafe_encode64(Bin) ->
-    Bin2 = base64:encode(Bin),
-    Bin3 = re:replace(binary_to_list(Bin2), "\\+", "-", [global, {return,list}]),
-    re:replace(Bin3, "/", "_", [global, {return,list}]).
-hash(Data)                  -> Hash = urlsafe_encode64(crypto:hash(sha, Data)),
-                               {substr(Hash, 1, 2), substr(Hash, 3)}.
+urlsafe_encode64(Bin)           -> Bin2 = base64:encode(Bin),
+                                   Bin3 = re:replace(binary_to_list(Bin2), "\\+", "-", [global, {return,list}]),
+                                   re:replace(Bin3, "/", "_", [global, {return,list}]).
+hash(Data)                      -> Hash = urlsafe_encode64(crypto:hash(sha, Data)),
+                                   {substr(Hash, 1, 2), substr(Hash, 3)}.
  
 % Given key, construct 2-part hash, directory / filename
-name(Verb, URI)             -> I = binary_to_list(Verb),
-                               hash(list_to_binary(I ++ URI)).
-name(Verb, URI, Req)        -> I = binary_to_list(Verb),
-                               hash(list_to_binary(I ++ URI ++ Req)).
+name(Verb, URI, Req)            -> I = binary_to_list(Verb),
+                                   hash(list_to_binary(I ++ URI ++ Req)).
 
-
-ensure_exists(Dir)          -> ensure_exists(Dir, file:list_dir(?ROOT ++ Dir)).
-
-ensure_exists(_,  {ok, _})  -> ok;
-ensure_exists(Dir, _)       -> file:make_dir(?ROOT ++ Dir).
+ensure_exists(Dir)              -> ensure_exists(Dir, file:list_dir(?ROOT ++ Dir)).
+ensure_exists(_,  {ok, _})      -> ok;
+ensure_exists(Dir, _)           -> file:make_dir(?ROOT ++ Dir).
            
 
-store({Dir, File}, Data)    -> ensure_exists(Dir),
-                               file:write_file(?ROOT ++ Dir ++ "/" ++ File, Data).
-store(Verb, URI, Data)      -> N = name(Verb, URI),
-                               store(N, Data).    
-store(Verb, URI, Req, Data) -> N = name(Verb, URI, Req),
-                               store(N, Data).
+store(Verb, URI, Req, Res, Hdr) -> {Dir, File} = name(Verb, URI, Req),
+                                   ensure_exists(Dir),
+                              	   Filename = ?ROOT ++ Dir ++ "/" ++ File,
+                                   {ok, _} = file:write_file(Filename ++ ".res", Res),
+							       file:write_file(Filename ++ ".nfo", Hdr).
 
-read({Dir, File})           -> file:read_file(?ROOT ++ Dir ++ "/" ++ File).
-read(Verb, URI)             -> N = name(Verb, URI),
-                                   {ok, Res}  = read(N++".res"),
-								   {ok, Meta} = read(N++".nfo"),
-								   {ok, Res, Meta}
-read(Verb, URI, Req)        -> read(Verb, URI++Req).
+read(Verb, URI, Req)            -> {Dir, File} = name(Verb, URI, Req),
+                                   N = ?ROOT ++ Dir ++ "/" ++ File,
+                                   {ok, Res}  = file:read_file(N++".res"),
+								   {ok, Meta} = file:read_file(N++".nfo"),
+								   {Meta2}    = jiffy:decode(Meta),
+								   {ok, Res, Meta2}.
 
--- src/mock_handler.erl
+++ src/mock_handler.erl
@@ -7,8 +7,6 @@
 
 -import(string, ['++'/2]).
 
-%-record(state, {}).
-
 init(_Type, Req, _Opts)   -> {ok, Req, no_state}.
 
 % Since cowboy, mucks up the URI, this gets it back
@@ -18,25 +16,20 @@ extract_uri(Req)          -> {Path, _} = cowboy_req:path(Req),
                              {QS, _}   = cowboy_req:qs(Req),
                              extract_uri(Path, QS).
 
-   URI          : a fully qualified URI
-   Status       : HTTP status of response
-   Content-Type : Content type of response (Can be omitted, freeform text, but has defaults)
-   Request      : Body of Request (only available to PUT and POST)
-   Response     : Mocked Response 
+% Delete meta info, remaining is assumed to be header response
+header(X) -> X2 = proplists:delete(<<"Status">>,  X ),
+             X3 = proplists:delete(<<"URI">>,     X2),
+             X4 = proplists:delete(<<"Request">>, X3),
+             X4.
+% Extract status from meta info
+status(X) ->  proplists:get_value(<<"Status">>, X).
    
 handle(Req, State) ->
-    URI = extract_uri(Req),
-    {Method, _} = cowboy_req:method(Req),
-    {_, Body,   _} = cowboy_req:body(Req),
-    % cowboy_req:body_length(Req) may prove helpful
-    {ok, Meta, Res} = mock:read(Method, URI, Body),
-    {ok, Req2} = cowboy_req:reply(200,
-        [
-            {<<"content-type">>, <<"text/plain">>}
-        ],
-        Res, Req),
+    URI             = extract_uri(Req),
+    {Method, _}     = cowboy_req:method(Req),
+    {_, Body, _}    = cowboy_req:body(Req),
+    {ok, Res, Meta} = mock:read(Method, URI, Body),
+    {ok, Req2}      = cowboy_req:reply(status(Meta), header(Meta), Res, Req),
     {ok, Req2, State}.
         
-
-terminate(_Reason, _Req, _State) ->
-    ok.
+terminate(_Reason, _Req, _State) -> ok.
