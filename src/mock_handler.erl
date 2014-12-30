0c9556bfa98f16e78c03ea3b43d37734092d68a2
Jonathan Hicks <jonathan.r.hicks@gmail.com>
2014-12-30 11:16:25 -0500

    Making the code a little more readable. Added jiffy as a requirement.

--- src/mock_handler.erl
+++ src/mock_handler.erl
@@ -1,29 +1,26 @@
+%%%-------------------------------------------------------------------
+%% @doc mockery http handler
+%% @end
+%%%-------------------------------------------------------------------
 -module(mock_handler).
 -behaviour(cowboy_http_handler).
 
--export([init/3]).
--export([handle/2]).
--export([terminate/3]).
+%% Application callbacks
+-export([init/3
+        ,handle/2
+        ,terminate/3]).
 
 -import(string, ['++'/2]).
 
-init(_Type, Req, _Opts)   -> {ok, Req, no_state}.
+%%====================================================================
+%% API
+%%====================================================================
 
-% Since cowboy, mucks up the URI, this gets it back
-extract_uri(Path, <<"">>) -> binary_to_list(Path);
-extract_uri(Path, QS)     -> binary_to_list(Path) ++ "?" ++ binary_to_list(QS).
-extract_uri(Req)          -> {Path, _} = cowboy_req:path(Req),
-                             {QS, _}   = cowboy_req:qs(Req),
-                             extract_uri(Path, QS).
+init(_Type, Req, _Opts) ->
+  {ok, Req, no_state}.
 
-% Delete meta info, remaining is assumed to be header response
-header(X) -> X2 = proplists:delete(<<"Status">>,  X ),
-             X3 = proplists:delete(<<"URI">>,     X2),
-             X4 = proplists:delete(<<"Request">>, X3),
-             X4.
-% Extract status from meta info
-status(X) ->  proplists:get_value(<<"Status">>, X).
-   
+%%--------------------------------------------------------------------
+
 handle(Req, State) ->
     URI             = extract_uri(Req),
     {Method, _}     = cowboy_req:method(Req),
@@ -31,5 +28,39 @@ handle(Req, State) ->
     {ok, Res, Meta} = mock:read(Method, URI, Body),
     {ok, Req2}      = cowboy_req:reply(status(Meta), header(Meta), Res, Req),
     {ok, Req2, State}.
-        
-terminate(_Reason, _Req, _State) -> ok.
+
+%%--------------------------------------------------------------------
+
+terminate(_Reason, _Req, _State) ->
+  ok.
+
+%%====================================================================
+%% Internal functions
+%%====================================================================
+
+% Since cowboy, mucks up the URI, this gets it back
+extract_uri(Path, <<"">>) ->
+  binary_to_list(Path);
+extract_uri(Path, QS) ->
+  binary_to_list(Path) ++ "?" ++ binary_to_list(QS).
+
+extract_uri(Req) ->
+  {Path, _} = cowboy_req:path(Req),
+  {QS, _}   = cowboy_req:qs(Req),
+  extract_uri(Path, QS).
+
+%%--------------------------------------------------------------------
+
+% Delete meta info, remaining is assumed to be header response
+header(X) ->
+  X2 = proplists:delete(<<"Status">>,  X ),
+  X3 = proplists:delete(<<"URI">>,     X2),
+  X4 = proplists:delete(<<"Request">>, X3),
+  X4.
+
+%%--------------------------------------------------------------------
+
+% Extract status from meta info
+status(X) ->
+  proplists:get_value(<<"Status">>, X).
+
--- src/mockery_app.erl
+++ src/mockery_app.erl
@@ -14,17 +14,19 @@
 %%====================================================================
 
 start(_StartType, _StartArgs) ->
-    Dispatch = cowboy_router:compile([
-        {'_', [{"/mockeries", mockeries_handler, []},
-           {'_',          mock_handler,      []}
-          ]
+  Dispatch = cowboy_router:compile([
+      {'_', [{"/mockeries", mockeries_handler, []}
+            ,{'_',          mock_handler,      []}
+            ]
       }
-    ]),
-    cowboy:start_http(my_http_listener, 100, [{port, 8080}],
-        [{env, [{dispatch, Dispatch}]}]
-    ),
+  ]),
+  cowboy:start_http(my_http_listener
+                    ,100
+                    ,[{port, 8080}]
+                    ,[{env, [{dispatch, Dispatch}]}]
+  ),
   file:set_cwd("../.."),
-    mockery_sup:start_link().
+  mockery_sup:start_link().
 
 %%--------------------------------------------------------------------
 
